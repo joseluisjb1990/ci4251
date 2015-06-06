b1f98982d0e1fff48a1f2760d466c29dfffce7c8
Jose Jimenez <jljb1990@gmail.com>
2015-06-06 12:45:30 -0430

    Terminada la parte de concurrencia

--- T3/tarea3.hs
+++ T3/tarea3.hs
@@ -1,12 +1,113 @@
 import System.Random
-import Control.Concurrent.STM
 import Control.Concurrent
 import Control.Monad
 import Data.Sequence as DS hiding (replicateM, take)
 import Control.Exception
-import Data.Typeable
 import System.Posix.Signals
+import Control.Concurrent.STM
 import qualified Data.Foldable as DF
+
+type Colador    = MVar Int
+type MBuffer a  = Chan a
+type MSem       = MVar Bool
+type MSeq a     = MVar (DS.Seq a)
+
+newMBuffer :: IO (MBuffer a)
+newMBuffer = newChan
+
+newMSeqInt :: Int -> IO(MSeq Int)
+newMSeqInt m = newMVar $ fromList $ take m $ repeat 0
+
+ateEmpanadaC :: MSeq Int -> Int -> IO()
+ateEmpanadaC si id =
+  do v <- takeMVar si
+     putMVar si (DS.adjust (+1) id v)
+
+mPut :: MBuffer a -> a -> IO()
+mPut = writeChan
+ 
+mGet :: MBuffer a -> IO a
+mGet = readChan
+
+mOutput buffer = 
+    do e <- mGet buffer
+       putStrLn $ e
+       mOutput buffer
+
+takeFromColador :: Colador -> MSem -> IO ()
+takeFromColador c s = 
+  do e <- takeMVar c
+     if    (e >  0) then putMVar c (e - 1)
+     else  do vC s
+              takeFromColador c s
+
+newColador :: Int -> IO Colador
+newColador = newMVar
+
+fillColador :: Colador -> Int -> IO ()
+fillColador c m = putMVar c m
+
+newMSem :: IO MSem
+newMSem = newEmptyMVar
+
+emptyColador :: Colador -> IO (Bool)
+emptyColador = isEmptyMVar
+
+vC :: MSem -> IO ()
+vC s = 
+    do tryPutMVar s True
+       return ()
+
+pC :: MSem -> IO (Bool)
+pC = takeMVar
+
+oneMoreRoundC :: Colador -> Int -> IO ()
+oneMoreRoundC c m =
+  do v <- takeMVar c
+     putMVar c (v + m)
+
+rafitaC c s m tr out =
+    do pC s
+       mPut out $ "Rafita está cocinando"
+       randomDelayR
+       fillColador c m
+       oneMoreRoundC tr m
+       mPut out $ "Rafita sirvió las empanadas"
+       rafitaC c s m tr out     
+
+parroquianoC id c s si out = 
+  do randomDelayP
+     mPut out $ "El parroquiano " ++ show id ++ " tiene hambre"
+     takeFromColador c s
+     ateEmpanadaC si id
+     mPut out $ "El parroquiano " ++ show id ++ " come empanada"
+     parroquianoC id c s si out
+
+putResumeC tid idp si tr = 
+    do  mapM_ killThread idp
+        r  <- takeMVar tr
+        putStrLn $ formatRafita r
+        pv <- takeMVar si
+        mapM_ putStr (formatParroquiano 0 pv)
+        putStrLn $ formatTotal pv
+        killThread tid
+
+classic' :: Int -> Int -> IO ()
+classic' m n =
+    do out <- newMBuffer
+       c   <- newColador m
+       tr  <- newColador m
+       s   <- newMSem
+       si  <- newMSeqInt n
+       idr <- forkIO (rafitaC c s m tr out)
+       idp <- forM [0..n-1] $ \i -> forkIO (parroquianoC i c s si out)
+       tid <- myThreadId
+       installHandler keyboardSignal (Catch (putResumeC tid (idr:idp) si tr)) Nothing
+       mOutput out
+
+
+classic :: Int -> Int -> IO ()
+classic m n = runSimulation classic' m n
 
 type Buffer a   = TVar (DS.Seq a)
 type Rafita     = TVar Int
@@ -46,9 +147,9 @@ output buffer =
        putStrLn $ str
        output buffer
 
-bufferToList :: Buffer a -> STM (DS.Seq a)
-bufferToList buffer = do ls <- readTVar buffer
-                         return $ ls
+readBufferInt :: Buffer a -> STM (DS.Seq a)
+readBufferInt buffer = do ls <- readTVar buffer
+                          return $ ls
 
 newRafita :: Int -> IO Rafita
 newRafita m = do r <- newTVarIO m
@@ -65,10 +166,10 @@ takeEmpanada r =
       if   m == 0 then retry
       else do writeTVar r (m - 1)
 
-oneMoreRound :: Rafita -> STM ()
-oneMoreRound r =
+oneMoreRound :: Int -> Rafita -> STM ()
+oneMoreRound n r =
   do  m <- readTVar r
-      writeTVar r (m + 1)
+      writeTVar r (m + n)
 
 putEmpanadas :: Int -> Rafita -> STM()
 putEmpanadas n r =
@@ -78,43 +179,40 @@ putEmpanadas n r =
 randomSeed :: Int
 randomSeed = 42
 
-classic :: Int -> Int -> IO ()
-classic m n = undefined
-
 ateEmpanada :: Int -> Buffer Int -> STM ()
-ateEmpanada id bi = do  s <- readTVar bi
-                        writeTVar bi (DS.adjust (+1) id s)
+ateEmpanada id bi = 
+  do  s <- readTVar bi
+      writeTVar bi (DS.adjust (+1) id s)
 
 parroquiano id r tr bi buffer = 
-    do  atomically $ put buffer ("El parroquiano " ++ show id ++ " tiene hambre") 
+    do  randomDelayP
+        atomically $ put buffer ("El parroquiano " ++ show id ++ " tiene hambre") 
         atomically $ takeEmpanada r
         atomically $ ateEmpanada id bi
         atomically $ put buffer ("El parroquiano " ++ show id ++ " esta comiendo")
 
-
-        l <- atomically $ thereIsEmpanada r      
-        if not l then
-          do  atomically $ v tr
-              randomDelayP
-              parroquiano id r tr bi buffer
-        else
-          do randomDelayP
-             parroquiano id r tr bi buffer
+        l <- atomically $ thereIsEmpanada r
+        if not l then atomically $ v tr
+        else return ()
+        
+        parroquiano id r tr bi buffer
 
 rafita n r e c buffer = 
     do atomically $ p e
        atomically $ put buffer ("Rafita está cocinando")
        randomDelayR
        atomically $ putEmpanadas n r
-       atomically $ oneMoreRound c
+       atomically $ oneMoreRound n c
        atomically $ put buffer ("Rafita sirvio las empanadas")
        rafita n r e c buffer
 
-randomDelayP = do r <- randomRIO (100000,700000)
-                  threadDelay r
+randomDelayP = 
+  do r <- randomRIO (100000,700000)
+     threadDelay r
 
-randomDelayR = do r <- randomRIO (300000,500000)
-                  threadDelay r
+randomDelayR = 
+  do r <- randomRIO (300000,500000)
+     threadDelay r
 
 formatParroquiano n l = 
     case viewl l of 
@@ -123,15 +221,15 @@ formatParroquiano n l =
 
 getRafita n = atomically $ readTVar n
  
-formatRafita r c = "\nRafita preparó " ++ show (r*c) ++ " empanadas"
+formatRafita c = "\nRafita preparó " ++ show c ++ " empanadas"
 
 formatTotal l = "\nTotal:   " ++ show(DF.sum l)
 
-putResume tid bi idp c r = 
-    do  forM_ idp $ \id -> killThread id
+putResume tid bi idp c = 
+    do  mapM_ killThread idp
         c' <- getRafita c
-        putStrLn $ formatRafita r c'
-        l  <- atomically $ bufferToList bi
+        putStrLn $ formatRafita c'
+        l  <- atomically $ readBufferInt bi
         mapM_ putStr (formatParroquiano 0 l)
         putStrLn $ formatTotal l
         killThread tid
@@ -139,56 +237,21 @@ putResume tid bi idp c r =
 transactional' m n = 
     do  out <- newBuffer
         r   <- newRafita m
-        c   <- newRafita 1
+        c   <- newRafita m
         e   <- newSem False
         bi  <- newEmptyBufferInt n
-        ids <- newBuffer
         tid <- myThreadId
-        atomically $ put ids tid
         idr <- forkIO (rafita m r e c out)
-        atomically $ put ids idr
         idp <- forM [0..n-1] $ \i -> forkIO (parroquiano i r e bi out)
-        forM_ idp $ \id -> atomically $ put ids id
-        installHandler keyboardSignal (Catch (putResume tid bi idp c m)) Nothing
+        installHandler keyboardSignal (Catch (putResume tid bi (idr:idp) c)) Nothing
         output out
 
 transactional :: Int -> Int -> IO ()
-transactional m n = catch (transactional' m n) 
+transactional m n = runSimulation transactional' m n
+
+runSimulation :: (Int -> Int -> IO ()) -> Int -> Int -> IO ()
+runSimulation f m n = catch (f m n) 
                           (\e -> case e of 
                                    ThreadKilled -> putStrLn "" 
-                                   otherwise    -> putStrLn "Ocurrió un error inesperado en el programa")
-
--- Cada parroquiano se ejcuta en un hilo distinto y cuando se acaban las empanadas
--- generan una excepcion. El hilo principal (rafita) atrapa la excepcion y prepara mas
--- empanadas.
-
--- transactional :: Int -> Int -> IO ()
--- transactional m n = do e <- newRafita m
---                        parroquiano e
---                
---                     -- Arranco los parroquianos. Cada parroquiano piensa un tiempo random y toma una empanada
--- 
--- parroquiano :: Rafita -> IO ()
--- parroquiano r = forever ( do t <- randomR (1,7) randomSeed
---                              threadDelay (fst t)
---                              e <- readTVar r
---                              writeTVar (e - 1)
---                         )
--- 
--- newRafita :: Int -> STM Rafita
--- newRafita m = do r <- newTVar m
---                  -- always (do epn <- readTVar r
---                  --            return (epn >= 0))
---                  return r  
-
-
---
---data NoMoreEmpanadas = NoMoreEmpanadas
- --   deriving(Show, Typeable)
-
---instance Exception NoMoreEmpanadas
-
--- thereIsEmpanada :: Rafita -> STM (Bool)
--- thereIsEmpanada r = 
---  do  m <- readTVar r
---      return $ m /= 0 
+                                   otherwise    -> putStrLn $ "Ocurrió un error inesperado en el programa " ++ show e
+                          )
